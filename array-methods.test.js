51a79ac6c55f91c0c82051622dfcf10a5d39585a
Katherine Tam <katherinemtam@gmail.com>
2021-06-08 22:19:26 -0700

    reduce test

--- array-methods.test.js
+++ array-methods.test.js
@@ -1,4 +1,4 @@
-import { map, filter, findIndex } from './array-methods.js';
+import { map, filter, findIndex, reduce } from './array-methods.js';
 
 describe('map(arr, callback)', () => {
 
@@ -41,13 +41,37 @@ describe('filter(arr, callback)', () => {
 
 describe('findIndex(arr, callback)', () => {
 
-  test.only('returns the index of the found item, -1 if no item is found', () => {
+  test('returns the index of the found item, -1 if no item is found', () => {
 
     const arr = [0, 1, 2, 3, 4, 5];
 
     const index = findIndex(arr, (item) => item === 2);
     
     expect(index).toEqual(2);
+
+  });
+});
+
+describe('reduce(arr, callback [, initialValue]', () => {
+
+  test('returns the final accumulator value WITHOUT an initial value', () => {
+
+    const arr = [1, 2, 3, 4, 5];
+
+    const accumulator = reduce(arr, (accumulator, item) => accumulator + item, 0);
+
+    expect(accumulator).toEqual(15);
+
+  });
+
+  test.only('returns the final accumulator value WITH an initial value', () => {
+
+    const arr = [1, 2, 3, 4, 5];
+    const initialValue = 5;
+
+    const accumulator = reduce(arr, (accumulator, item) => accumulator + item, initialValue);
+
+    expect(accumulator).toEqual(20);
 
   });
 });
